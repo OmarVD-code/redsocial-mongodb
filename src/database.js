b81c5e94b5f64833c6a411304580e8e4e80cacd1
Omar-Villarreal <64047589+OmarVD-code@users.noreply.github.com>
2023-01-03 16:58:36 -0500

    update connection

--- src/database.js
+++ src/database.js
@@ -1,10 +1,7 @@
 const mongoose = require('mongoose');
 // const { database } = require('./keys.js');
 
-mongoose.connect("mongodb+srv://omarvillarreal2000:mymongopass@cluster0.bu0gh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
-	useNewUrlParser: true,
-	useUnifiedTopology: true
-})
+mongoose.connect("mongodb+srv://omarvillarreal2000:mymongopass@cluster0.bu0gh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 	.then(db => console.log('DB is connected'))
 	.catch(err => console.error(err));
 
